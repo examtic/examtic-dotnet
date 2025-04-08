74b759011c0fb88f65a9743e581044792db65c7b
Alex <alexspopov1@gmail.com>
2025-04-08 18:51:15 +0300

    initial commit

---
+++ ExamticService.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+using System.Text.Json.Nodes;
+
+namespace Examtic;
+
+public class ExamticService(IConfiguration configuration)
+{
+    public string GetLoginToken(string userId, bool autoCreate = false)
+    {
+        var secret = configuration["Examtic:Secret"];
+
+        if (string.IsNullOrEmpty(secret))
+        {
+            throw new Exception("Secret missing in configuration.");
+        }
+
+        return GetLoginToken(userId, secret, autoCreate);
+    }
+
+    public static string GetLoginToken(string userId, string secret, bool autoCreate = false)
+    {
+        var json = new JsonObject
+        {
+            ["userId"] = userId,
+            ["autoCreate"] = autoCreate
+        };
+
+        var aes = Aes.Create();
+        var encryptor = aes.CreateEncryptor(Encoding.UTF8.GetBytes(secret), aes.IV);
+
+        using var memoryStream = new MemoryStream();
+        using var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+
+        using (var streamWriter = new StreamWriter(cryptoStream))
+        {
+            streamWriter.Write(json.ToJsonString());
+        }
+
+        var iv = aes.IV;
+        var encrypted = memoryStream.ToArray();
+
+        return Convert.ToBase64String(iv.Concat(encrypted).ToArray());
+    }
+}
+
---
+++ Extensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Examtic;
+
+public static class Extensions
+{
+    public static IServiceCollection AddExamtic(this IServiceCollection services)
+    {
+        return services.AddScoped<ExamticService>();
+    }
+}
